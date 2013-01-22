43142840cfb8a092443de466c28488ef02da53fc
Yingying Chen <yingying.chen@rea-group.com>
2013-01-22 16:43:57 +0800

    extract instance variable for cartest.

--- src/com/tw/test/CarTest.java
+++ src/com/tw/test/CarTest.java
@@ -1,6 +1,7 @@
 package com.tw.test;
 
 import com.tw.car.Car;
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
@@ -14,36 +15,38 @@ import static junit.framework.Assert.assertEquals;
  */
 public class CarTest {
 
+    private Car car;
+
+    @Before
+    public void setup() {
+        car = new Car();
+    }
+
     @Test
     public void shouldBeDownWhenGetInCar() {
-        Car car = new Car();
         assertEquals(car.display(), "已熄火");
     }
 
     @Test
     public void shouldBeDownWhenDownAndStepGas() {
-        Car car = new Car();
         car.stepGas();
         assertEquals(car.display(), "已熄火");
     }
 
     @Test
     public void shouldBeDownWhenDownAndGear() {
-        Car car = new Car();
         car.gear();
         assertEquals(car.display(), "已熄火");
     }
 
     @Test
     public void shouldBeFiredWhenFire() {
-        Car car = new Car();
         car.fire();
         assertEquals(car.display(), "已启动");
     }
 
     @Test
     public void shouldBeGearedWhenFireAndGear() {
-        Car car = new Car();
         car.fire();
         car.gear();
         assertEquals(car.display(), "已挂档");
@@ -51,7 +54,6 @@ public class CarTest {
 
     @Test
     public void shouldBeIdlyWhenFireAndStepGas() {
-        Car car = new Car();
         car.fire();
         car.stepGas();
         assertEquals(car.display(), "已空转");
@@ -59,7 +61,6 @@ public class CarTest {
 
     @Test
     public void shouldBeMovedWhenFireAndGearAndStepGas() {
-        Car car = new Car();
         car.fire();
         car.gear();
         car.stepGas();
